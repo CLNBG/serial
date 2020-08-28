0ae2b4d6a26ca6df6a3336647065de72fef79a1f
guoliang <clnanobio@outlook.com>
2020-08-28 10:15:00 +0800

    update

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "serialCom"
+    kind "StaticLib"
+    language "C++"
+
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    includedirs 
+    {
+        "include",
+        "src"
+    }
+
+    files
+    {
+        "src/serial.cpp",
+        "src/impl/win.cpp",
+        "src/impl/list_ports/list_ports_win.cpp",
+        "include/serial/serial.h",
+        "include/serial/v8stdint.h",
+        "include/serial/impl/win.h"
+    }
+
+    links
+    {
+        "setupapi.lib"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "Off"
+
+
+    filter "configurations:Debug"
+        systemversion "latest"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        systemversion "latest"
+        runtime "Release"
+        optimize "on"
